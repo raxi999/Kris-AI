b45248b5d65ac97eeec3fa31d53af345f0cc7051
raxi999 <raxkumaer@gmail.com>
2025-05-23 08:31:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "Hello from Kris AI!" 
+    return "Kris AI is running from main.py!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
